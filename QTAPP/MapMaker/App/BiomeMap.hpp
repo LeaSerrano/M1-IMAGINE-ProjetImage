b5b037a75f8e2431c292d760c221d0b09829c24d
Léa <leaserrano1119@gmail.com>
2023-04-06 10:26:32 +0200

    Ajiut de l'avancée sur le biomes

--- QTAPP/MapMaker/App/BiomeMap.hpp
+++ QTAPP/MapMaker/App/BiomeMap.hpp
@@ -29,9 +29,9 @@ public:
     inline static Color desert_hills_color = Color(255,200,50);;
     inline static Color desert_plateau_color = Color(150,100,80);;
 
-    inline static Color ice_color = Color(135,206,250);;
-    inline static Color ice_hills_color = Color(0,191,255);;
-    inline static Color ice_plateau_color = Color(30,144,255);;
+    inline static Color ice_color = Color(250,250,255);;
+    inline static Color ice_hills_color = Color(188, 188, 209);;
+    inline static Color ice_plateau_color = Color(130, 130, 173);;
 
     static bool isTheSameColor(int x, int y, ImageBase* image, Color color) {
         if (image->get(x, y, 0) == color.r && image->get(x, y, 1) == color.g && image->get(x, y, 2) == color.b) {
@@ -46,38 +46,47 @@ public:
         int height = width;
         double scale = DataManager::instance->requestValue("map_scale");
 
-        double desert_temperature = DataManager::instance->requestValue("desert_temperature");
-        double ice_temperature = DataManager::instance->requestValue("ice_temperature");
-        //double climate = DataManager::instance->requestValue("climate");
+        float desert_temperature = Utilities::quantile(climateMap,0.7);
+        desert_temperature = DataManager::instance->requestValue("desert_temperature",desert_temperature,true);
 
-        ImageBase* biomeMap = new ImageBase(width,height,true);
+        float ice_temperature = Utilities::quantile(climateMap,0.7);
+        ice_temperature = DataManager::instance->requestValue("ice_temperature",ice_temperature,true);
+
+        float humidity = DataManager::instance->requestValue("humidity");
+
+
+        ImageBase* biomeMap = new ImageBase(width, height, true);
 
         for(int y = 0; y < height ; y++)
         {
             for(int x = 0; x < width; x++)
             { 
+                biomeMap->set(x, y, 0, reliefMap->get(x, y, 0));
+                biomeMap->set(x, y, 1, reliefMap->get(x, y, 1));
+                biomeMap->set(x, y, 2, reliefMap->get(x, y, 2));
+
                 if (isTheSameColor(x, y, reliefMap, ReliefMap::plain_color) && (double)climateMap->get(x, y, 0)/255 > desert_temperature) {
-                    reliefMap->setColor(x,y,desert_color);
+                    biomeMap->setColor(x,y,desert_color);
                 }
                 else if (isTheSameColor(x, y, reliefMap, ReliefMap::hills_color) && (double)climateMap->get(x, y, 0)/255 > desert_temperature) {
-                    reliefMap->setColor(x,y,desert_hills_color);
+                    biomeMap->setColor(x,y,desert_hills_color);
                 }
                 else if (isTheSameColor(x, y, reliefMap, ReliefMap::plateau_color) && (double)climateMap->get(x, y, 0)/255 > desert_temperature) {
-                    reliefMap->setColor(x,y,desert_plateau_color);
+                    biomeMap->setColor(x,y,desert_plateau_color);
                 }
                 else if (isTheSameColor(x, y, reliefMap, ReliefMap::plain_color) && (double)climateMap->get(x, y, 0)/255 < ice_temperature) {
-                    reliefMap->setColor(x,y,ice_color);
+                    biomeMap->setColor(x,y,ice_color);
                 }
                 else if (isTheSameColor(x, y, reliefMap, ReliefMap::hills_color) && (double)climateMap->get(x, y, 0)/255 < ice_temperature) {
-                    reliefMap->setColor(x,y,ice_hills_color);
+                    biomeMap->setColor(x,y,ice_hills_color);
                 }
                 else if (isTheSameColor(x, y, reliefMap, ReliefMap::plateau_color) && (double)climateMap->get(x, y, 0)/255 < ice_temperature) {
-                    reliefMap->setColor(x,y,ice_plateau_color);
+                    biomeMap->setColor(x,y,ice_plateau_color);
                 }
 
             }
         }
 
-        return reliefMap;
+        return biomeMap;
     }
 };
